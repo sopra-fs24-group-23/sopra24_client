d7cb9be01291ead17e3b02c9a3ff1b3d1d69560c
fr4n715k4 <108323504+fr4n715k4@users.noreply.github.com>
2024-03-25 22:54:29 +0100

    Setup Registration view (not closed)
    Setup Registration view (not yet closing task)

--- src/components/views/Login.tsx
+++ src/components/views/Login.tsx
@@ -5,33 +5,7 @@ import {useNavigate} from "react-router-dom";
 import { Button } from "components/ui/Button";
 import "styles/views/Login.scss";
 import BaseContainer from "components/ui/BaseContainer";
-import PropTypes from "prop-types";
-
-/*
-It is possible to add multiple components inside a single file,
-however be sure not to clutter your files with an endless amount!
-As a rule of thumb, use one file per component and only add small,
-specific components that belong to the main one in the same file.
- */
-const FormField = (props) => {
-  return (
-    <div className="login field">
-      <label className="login label">{props.label}</label>
-      <input
-        className="login input"
-        placeholder="enter here.."
-        value={props.value}
-        onChange={(e) => props.onChange(e.target.value)}
-      />
-    </div>
-  );
-};
-
-FormField.propTypes = {
-  label: PropTypes.string,
-  value: PropTypes.string,
-  onChange: PropTypes.func,
-};
+import FormField from "components/ui/FormField";
 
 const Login = () => {
   const navigate = useNavigate();
