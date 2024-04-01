fc0805477016bb443a206e9dab0047db9e72c8a4
fr4n715k4 <108323504+fr4n715k4@users.noreply.github.com>
2024-04-01 22:32:04 +0200

    Adjusted Lobby Layout

--- src/components/views/Lobby.tsx
+++ src/components/views/Lobby.tsx
@@ -1,15 +1,41 @@
 import BackgroundImageLobby from "styles/views/BackgroundImageLobby";
-import React from "react";
+import React, { useState } from "react";
 import { Spinner } from "../ui/Spinner";
+import CustomButton from "components/ui/CustomButton";
+import { useNavigate, useParams } from "react-router-dom";
+import { Box, Typography, List, ListItem } from "@mui/material";
 
 const Lobby = () => {
+  const { lobbyId } = useParams();
+  const [players, setPlayers] = useState([]);
+  const [lobbyDetails, setLobbyDetails] = useState(null);
 
 
   let content = <Spinner />;
 
   return (
     <BackgroundImageLobby>
-      {content}
+      <Box sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#e0e0e0", // Grey
+        padding: "20px",
+        borderRadius: "10px",
+        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+        width: "80%",
+        maxWidth: "500px",
+        height: "60%",
+      }}>
+        <Typography variant="h4" gutterBottom sx={{ fontFamily: "Londrina Solid"}}>
+          Players
+        </Typography>
+        <List sx={{
+          width: "100%",
+        }}>
+        </List>
+      </Box>
     </BackgroundImageLobby>
   );
 };
--- src/styles/views/BackgroundImageLobby.tsx
+++ src/styles/views/BackgroundImageLobby.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import PropTypes from "prop-types";
+import "../../styles/ui/main.scss";
 
 const BackgroundImageLobby = ( {children}) => {
   return (
     <Box
+      className="background-image-lobby"
       sx={{
         backgroundImage: "url('/BackgroundImages/Lobby.png')",
         backgroundPosition: "center",
@@ -13,6 +15,10 @@ const BackgroundImageLobby = ( {children}) => {
         width: "100vw", // 100% of the viewport width
         height: "100vh", // 100% of the viewport height
         position: "relative", // Changed to relative for stacking content
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
       {children}
