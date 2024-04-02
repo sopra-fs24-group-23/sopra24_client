feefd1bae6fd9738603dcd1a9b1973c131569a46
fr4n715k4 <108323504+fr4n715k4@users.noreply.github.com>
2024-04-03 00:13:31 +0200

    Continue with lobby layout

--- src/components/views/Lobby.tsx
+++ src/components/views/Lobby.tsx
@@ -9,12 +9,18 @@ const Lobby = () => {
   const { lobbyId } = useParams();
   const [players, setPlayers] = useState([]);
   const [lobbyDetails, setLobbyDetails] = useState(null);
+  const navigate = useNavigate();
+
+  const goToEditSettings = () => {
+    navigate("/editSettings");
+  };
 
 
   let content = <Spinner />;
 
   return (
     <BackgroundImageLobby>
+      {/* Outer box */}
       <Box sx={{
         display: "flex",
         flexDirection: "column",
@@ -25,19 +31,45 @@ const Lobby = () => {
         borderWidth: "2px",
         borderStyle: "solid",
         padding: "20px",
-        borderRadius: "10px",
+        borderRadius: "27px",
         boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
-        width: "80%",
+        width: "35%",
         maxWidth: "500px",
-        height: "60%",
+        height: "77%",
+        margin: "auto",
+        position: "relative",
       }}>
-        <Typography variant="h4" gutterBottom sx={{ fontFamily: "Londrina Solid"}}>
-          Players
-        </Typography>
-        <List sx={{
-          width: "100%",
+        {/*
+        <CustomButton onClick={() => navigate(`/lobbies/${response.data.id}`)}>
+            Instructions
+    </CustomButton> */}
+        {/* Inner box for player list*/}
+        <Box sx={{
+          backgroundColor: "#e0e0e0",
+          borderColor: "black",
+          borderWidth: "2px",
+          borderStyle: "solid",
+          width: "60%",
+          height: "60%",
+          margin: "auto",
+          padding: "20px",
+          borderRadius: "10px",
+          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+          position: "relative",
+          top: "-8%",
         }}>
-        </List>
+          <Typography variant="h4" gutterBottom
+            sx={{
+              fontFamily: "Londrina Solid",
+              textAlign: "center",
+            }}>
+            Players
+          </Typography>
+          <List sx={{
+            width: "100%",
+          }}>
+          </List>
+        </Box>
       </Box>
     </BackgroundImageLobby>
   );
