347816098d9f3d1d349b10cf3d5f6d0f61069d5b
Dedphish <jonas@krumm.ch>
2024-04-09 12:07:52 +0200

    wrapped app in websocketcontext

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import AppRouter from "./components/routing/routers/AppRouter";
+import WebSocketProvider from "./components/providers/WebSocketProvider";
 
-/**
- * Happy coding!
- * React Template by Lucas Pelloni
- * Overhauled by Kyrill Hux
- * Updated by Marco Leder
- */
+
 const App = () => {
-  return (
-    <div>
-      <AppRouter />
-    </div>
-  );
+    return (
+        <WebSocketProvider>
+            <div>
+                <AppRouter/>
+            </div>
+        </WebSocketProvider>
+    );
 };
 
 export default App;
