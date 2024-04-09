946730397ca10056a21cec107269bc06f9c33299
Dedphish <jonas@krumm.ch>
2024-04-09 12:56:03 +0200

    updated connect function to return Promise

--- src/components/providers/WebSocketProvider.tsx
+++ src/components/providers/WebSocketProvider.tsx
@@ -12,31 +12,36 @@ const WebSocketProvider = ({ children }) => {
     const stompClient = useRef(null);
     const subscriptionRequests = useRef(new Map())
 
+    /** Connect function, returns promise that is only resolved after successful connection,
+     * otherwise rejected. Does nothing if the client is already initialized and connected**/
     const connect = ({ sessionId }) => {
-        // if there is no active client yet
-        if(!stompClient.current) {
-            aSessionId.current = sessionId;
-            // client setup
-            stompClient.current = new Client({
-                brokerURL: `${getDomain()}ws`, // might need to change to "/ws" here
-                heartbeatIncoming: 10000,
-                heartbeatOutgoing: 10000,
-                reconnectDelay: 5000,
-                debug: isProduction() ? undefined : (str) => console.log(str)
-            })
-            // enable debug logging  in dev environment
-            if(!isProduction()) {
-                stompClient.current.debug = (str) => { console.log(str); }
-            }
-            // (re-)subscribe all subscriptions on connection
-            stompClient.current.onConnect = () => {
-                subscriptionRequests.current.forEach((request) => {
-                    stompClient.current.subscribe(request.destination, request.callback)
+        return new Promise((resolve, reject) => {
+            if (!stompClient.current) {
+                aSessionId.current = sessionId;
+                // client setup
+                stompClient.current = new Client({
+                    brokerURL: `${getDomain()}ws`, // might need to change to "/ws" here
+                    heartbeatIncoming: 10000,
+                    heartbeatOutgoing: 10000,
+                    reconnectDelay: 5000,
+                    debug: isProduction() ? undefined : (str) => console.log(str),
+                    beforeConnect: isProduction() ? undefined : () => console.log("Connecting websocket."),
+                    onStompError: (frame) => {
+                        `Encountered a StompError: ${frame}`
+                    }
                 })
+                // on connection: (re-)subscribe all subscriptions and resolve promise
+                stompClient.current.onConnect = () => {
+                    subscriptionRequests.current.forEach((request) => {
+                        stompClient.current.subscribe(request.destination, request.callback)
+                    })
+                    resolve()
+                }
             }
-        }
-        // finally, activate the client
-        stompClient.current.activate()
+            else if(stompClient.current && !stompClient.current.active){
+                stompClient.current.activate()
+            }
+        })
     }
 
     const disconnect = () => {
@@ -45,10 +50,10 @@ const WebSocketProvider = ({ children }) => {
 
     /** method to send a body to an app-destination in the BE
      * NOTICE: this method calls JSON.stringify() on the body **/
-    function send ({ destination, body }) {
-        if(stompClient.current && stompClient.current.active()) {
+    function send ( destination: string, body: any ) {
+        if(stompClient.current && stompClient.current.active) {
             stompClient.current.publish({
-                destination: `${destination}`,
+                destination: destination,
                 body: `${JSON.stringify(body)}`} // here :)
             )
         }
@@ -56,9 +61,14 @@ const WebSocketProvider = ({ children }) => {
 
     /** Method to subscribe to a topic/queue destination in the BE
      * NOTICE: method is called subscribeClient to avoid confusion
-     * with stompClient.subscribe **/
+     * with stompClient.subscribe
+     * subscriptionRequest names must be unique**/
     function subscribeClient ( subscriptionRequest : StompSubscriptionRequest) {
-        if(stompClient.current && stompClient.current.active()) {
+        if(subscriptionRequests.current.has(subscriptionRequest.name)) {
+            console.log("You tried to add a subscriptionRequest with a duplicate name, the names must be unique")
+            alert("Cannot create two subscriptionRequests with the same name.")
+        }
+        else if(stompClient.current && stompClient.current.active) {
             // subscribe client, store subscription to request object for unsubscribing
             subscriptionRequest.subscription =
                 stompClient.current.subscribe(
@@ -76,7 +86,7 @@ const WebSocketProvider = ({ children }) => {
     }
 
     function unsubscribeClient ( name : string ) {
-        if(stompClient.current && stompClient.current.active()) {
+        if(stompClient.current && stompClient.current.active) {
             // retrieve request from requestList
             const request = subscriptionRequests.current.get(name)
             // unsubscribe
