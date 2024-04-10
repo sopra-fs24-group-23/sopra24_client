df6a7cf8803515e32ffcf1de416fbaf42d4f26cb
Dedphish <jonas@krumm.ch>
2024-04-10 13:25:10 +0200

    added sessionId attribute

--- src/interfaces/StompSubscriptionRequest.tsx
+++ src/interfaces/StompSubscriptionRequest.tsx
@@ -2,6 +2,7 @@ import { StompSubscription } from "@stomp/stompjs";
 
 export default interface StompSubscriptionRequest {
   destination: string,
+  sessionId: string,
   callback: Function
   subscription?: StompSubscription
 }
