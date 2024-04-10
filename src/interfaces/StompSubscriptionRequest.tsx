9ae1d7292f34f1a352ef485d5b5d98fb2472b2ab
Dedphish <jonas@krumm.ch>
2024-04-10 11:45:11 +0200

    removed name attribute, changed file extension ts->tsx

--- src/interfaces/StompSubscriptionRequest.tsx
+++ src/interfaces/StompSubscriptionRequest.tsx
@@ -1,7 +1,6 @@
 import { StompSubscription } from "@stomp/stompjs";
 
 export default interface StompSubscriptionRequest {
-  name: string,
   destination: string,
   callback: Function
   subscription?: StompSubscription
