3dfd4c2de6cf05ac7db5bb3d3ba0d11a58ed5a63
Dedphish <jonas@krumm.ch>
2024-04-26 12:50:54 +0200

    define model types, adjust models

--- src/components/providers/GameSettingsProvider.tsx
+++ src/components/providers/GameSettingsProvider.tsx
@@ -1,17 +1,18 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useState } from "react";
 import GameSettingsContext from "../../contexts/GameSettingsContext";
-import GameSettings from "../../models/GameSettings";
+import GameSettingsModel from "../../models/GameSettings";
+import { GameSettings } from "../../types"
 import PropTypes from "prop-types";
 import { isProduction } from "../../helpers/isProduction";
 
 const GameSettingsProvider = ({ children }) => {
-  const [gameSettings, setGameSettings] = useState(new GameSettings());
+  const [gameSettings, setGameSettings] = useState<GameSettings>(new GameSettingsModel());
 
-  const setGameSettingsVariable = (gameSettingsData) => {
+  const setGameSettingsVariable = (gameSettingsData: any) => {
     if (!isProduction()) {
       console.log(`DEBUG setting gameSettings in context to: ${JSON.stringify(gameSettingsData)}`)
     }
-    setGameSettings(new GameSettings(gameSettingsData))
+    setGameSettings(new GameSettingsModel(gameSettingsData))
   }
 
   return (
