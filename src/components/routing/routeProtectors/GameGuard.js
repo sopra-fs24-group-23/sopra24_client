a6a5a6cb4f6de3a0ec78697f970a7c350249b8e1
fr4n715k4 <108323504+fr4n715k4@users.noreply.github.com>
2024-04-26 15:44:46 +0200

    Add Dialog in GameGuard

--- src/components/routing/routeProtectors/GameGuard.js
+++ src/components/routing/routeProtectors/GameGuard.js
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect } from "react";
-import {Navigate, Outlet} from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import PropTypes from "prop-types";
 import { api } from "../../../helpers/api";
 import UserContext from "../../../contexts/UserContext";
 import User from "../../../models/User";
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from "@mui/material";
+import CustomButton from "components/ui/CustomButton";
 
 /**
  * routeProtectors interfaces can tell the router whether or not it should allow navigation to a requested route.
@@ -17,24 +19,49 @@ export const GameGuard = () => {
   const { user, setUser } = useContext(UserContext)
   const token = localStorage.getItem("token")
   const id = localStorage.getItem("id")
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     if (token && id && user === null) {
       const fetchUser = async () => {
-        const response = await api.get("/users/" + id);
-        setUser(new User(response.data))
+        try {
+          const response = await api.get("/users/" + id);
+          setUser(new User(response.data))
+        } catch (e) {
+          console.error("There was an error fetching the user: ", e);
+          setOpen(true);
+        }
       }
-      try {
-        fetchUser()
-      }
-      catch (e) {
-        alert("There was an error fetching the user, you might need to log-in or register again.")
-      }
+        fetchUser();
     }
-  }, [token, id])
+  }, [token, id]);
 
   if (token && id) {
-    return <Outlet />;
+    return (
+      <>
+        <Outlet />
+        <Dialog
+          open={open}
+          onClose={handleClose}
+        >
+          <DialogTitle>{"Error"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              There was an error fetching the user, you might need to log-in or register again.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <CustomButton onClick={handleClose}>
+              Close
+            </CustomButton>
+          </DialogActions>
+        </Dialog>
+      </>
+    );
   }
   else {
     return <Navigate to="/login" replace />;
