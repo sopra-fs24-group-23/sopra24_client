9633f6a480c7072d851cedd0a060912e2103866c
fr4n715k4 <108323504+fr4n715k4@users.noreply.github.com>
2024-05-02 17:02:53 +0200

    Fix scrolling in the VotingResults view

--- src/components/views/VotingResults.tsx
+++ src/components/views/VotingResults.tsx
@@ -78,13 +78,14 @@ const VotingResults = () => {
         borderWidth: "2px",
         borderStyle: "solid",
         width: "60%",
-        height: "60%",
+        height: "auto",
         margin: "auto",
         padding: "20px",
         borderRadius: "10px",
         boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
         position: "relative",
         top: "10px",
+        overflowY: "auto",
       }}>
         <Typography variant="h4" gutterBottom sx={{
           fontFamily: "Londrina Solid",
