edd7b7afe594de76dacd2e82ea4e1d44a6e1fa13
nilshgt <heget.nils@gmail.com>
2024-05-20 15:52:27 +0200

    work on issue #138

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import UserProvider from "./components/providers/UserProvider";
 import GameStateProvider from "./components/providers/GameStateProvider";
 import GameSettingsProvider from "./components/providers/GameSettingsProvider";
 import ChatProvider from "./components/providers/ChatProvider";
+import ConsentBanner from "./components/ui/ConsentBanner";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
             <GameSettingsProvider>
               <div>
                 <AppRouter />
+                <ConsentBanner onConsent={() => console.log('Consented')} onDecline={() => console.log('Declined')} />
               </div>
             </GameSettingsProvider>
           </GameStateProvider>
